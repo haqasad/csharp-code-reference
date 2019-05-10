46a5791c9411828b0d2f9c0e45b4c8d9f0470a2b
haqasad <haq.asad000@gmail.com>
2019-05-10 16:06:45 -0400

    Create project structure
    Add console project EasyRef.ExecuteOop in sub-module Oop

---
+++ sources/Oop/EasyRef.ExecuteOop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasyRef.ExecuteOop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
