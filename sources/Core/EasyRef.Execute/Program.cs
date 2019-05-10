a0243b25db1be545b7df750efe6e8c8d8bf86ca5
haqasad <haq.asad000@gmail.com>
2019-05-10 02:28:26 -0400

    Create project structure
    Added EasyRef.Execute console application to Core sub-module

---
+++ sources/Core/EasyRef.Execute/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasyRef.Execute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
